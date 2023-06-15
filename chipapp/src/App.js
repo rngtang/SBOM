f391cb4d235c7ee5c3b54a1965be4b7de065b3a1
Judy Chen <jc942@duke.edu>
2023-06-15 16:58:18 -0400

    resolved merge

--- chipapp/src/App.js
+++ chipapp/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import SyftParser from './SyftParser';
-<<<<<<< HEAD
 import CodeVisualizer from './CodeVisualizer';
 import SendJSON from './SendJSON';
-=======
 import { CodeVisualizer } from './CodeVisualizer';
 
->>>>>>> bb45b5dcdf087217fa8ed9d856aa657b47d5e74f
 
 const App = () => {
   return (
     <div>
       {/* Other components and content */}
       <SendJSON />
-      <SyftParser />
+      {/* <SyftParser /> */}
       <CodeVisualizer />
     </div>
   );
