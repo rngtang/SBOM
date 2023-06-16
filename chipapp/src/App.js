f1dfb74a952ed4288d15edb2b6e72d7b87afa789
James Mu <jamesmu@Jamess-MacBook-Air.local>
2023-06-16 09:55:34 -0400

    basic d3 is up in react

---
+++ chipapp/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import D3Visualization from './D3Visualization';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Chipapp v1.0</h1>
+      <D3Visualization />
+    </div>
+  );
+};
+
+export default App;
