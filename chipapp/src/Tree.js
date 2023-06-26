e6ec46b526258b08afe3675dcdddeed837ef0ec3
James Mu <jamesmu@Jamess-MacBook-Air.local>
2023-06-26 10:39:08 -0400

    doesn't work yet, tried converting to cyclone format

--- chipapp/src/Tree.js
+++ chipapp/src/Tree.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tree as D3Tree } from 'react-d3-tree';
 import importData from './data.json';
 
 const Tree = () => {
-
   const treeConfig = {
     enableLegacyTransitions: true,
     separation: { siblings: 0.75, nonSiblings: 0.75 },
     dimensions: { height: 500, width: 500 }
     // the dimensions are kinda hard coded lol
   };
+  const [treeData, setTreeData] = useState(null);
 
-  return (
-    <div style={{ width: '100%', height: '650px' }}>
-      <D3Tree data={importData} {...treeConfig} />
-    </div>
-  );
+  useEffect(() => {
+    const convertedData = {
+      name: 'root',
+      children: importData.components.map((component) => ({
+        name: component.name,
+        attributes: component.licenses.map((license) => ({
+          name: license.name,
+        })),
+      })),
+    };
+
+    setTreeData(convertedData);
+  }, []);
+
+  return (<div style={{ width: '100%', height: '650px' }}>
+    <D3Tree data={treeData} {...treeConfig} />
+  </div>);
 };
 
 export default Tree;
+
+// import React from 'react';
+// import { Tree as D3Tree } from 'react-d3-tree';
+// import importData from './data.json';
+
+// const Tree = () => {
+
+//   const treeConfig = {
+//     enableLegacyTransitions: true,
+//     separation: { siblings: 0.75, nonSiblings: 0.75 },
+//     dimensions: { height: 500, width: 500 }
+//     // the dimensions are kinda hard coded lol
+//   };
+
+//   return (
+//     <div style={{ width: '100%', height: '650px' }}>
+//       <D3Tree data={importData} {...treeConfig} />
+//     </div>
+//   );
+// };
+
+// export default Tree;
