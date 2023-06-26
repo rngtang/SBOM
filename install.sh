0d10b5b8a121a17d564125c8cf898d8c75579fb3
Judy Chen <jc942@duke.edu>
2023-06-26 13:37:08 -0400

    weird

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+whoami
+date
+
 echo "Hello world"
 
+chmod +x install.sh
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b 
-
 sudo cp bin/syft /usr/local/bin
-
 syft -h 
 
 echo "DONE"
