a62da1e9b901d4d517909f2ba7214fcc96c4a9c2
James Mu <jamesmu@Jamess-MacBook-Air.local>
2023-06-26 14:06:48 -0400

    started text in home and generate pages

--- chipReact/src/pages/GenerateSBOMs.js
+++ chipReact/src/pages/GenerateSBOMs.js
@@ -1,4 +1,35 @@
-function GenerateSBOMs(){
-    return <div className='page'>GenerateSBOMs</div>
-}
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Prereqs = () => {
+    return (
+        <div>
+            <h3>Prerequisites</h3>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur metus vitae volutpat mollis. Cras vestibulum, nisi vel placerat accumsan, nisl lectus pharetra ex, vitae auctor ligula velit ac purus. Ut sodales nulla lectus, ut dictum ante consequat nec. Phasellus viverra ex tempus velit venenatis, ac facilisis sapien facilisis. Aliquam et mauris lacinia quam rutrum pellentesque at consequat dolor. Maecenas at tempus sem. Aliquam in magna non urna facilisis tincidunt. Sed nec dolor ligula. Nunc lorem odio, pulvinar quis est eget, commodo varius lacus.
+            </p>
+        </div>
+    );
+};
+
+const WhatAreSBOMs = () => {
+    return (
+        <div className="what-sboms-section">
+            <h3>What are SBOMs?</h3>
+            <p>
+                A Software Bill of Materials (SBOM) is a formal record of all software packages and components used by a software project or machine.
+                These components can be commercial or open-source, and in turn rely on other components themselves. This creates a series of reliances, or a chain of dependencies, that is not easily visible to the creator or user of the main project.
+            </p>
+        </div>
+    );
+};
+
+const GenerateSBOMs = () => {
+    return (
+        <Layout>
+            <Prereqs />
+        </Layout>
+    );
+};
+
 export default GenerateSBOMs;
