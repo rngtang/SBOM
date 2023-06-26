9f0654bd60e70cce82d3ce62234eeafce93fac70
Judy Chen <jc942@duke.edu>
2023-06-26 14:17:22 -0400

    trying to remove sudo

--- linux_install.sh
+++ linux_install.sh
@@ -7,14 +7,10 @@ chmod +x linux_install.sh
 # curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh 
 wget https://raw.githubusercontent.com/anchore/syft/main/install.sh
 
-echo "installed??"
-
 chmod +x install.sh
 ./install.sh
 
-cd bin 
-
-sudo mv ./syft /usr/local/bin
+sudo mv ./bin/syft /usr/local/bin
 syft -h 
 
 echo "DONE"
