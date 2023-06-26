3de6cf36666d51f738db881ce9eebce3ca470b2b
James Mu <jamesmu@Jamess-MacBook-Air.local>
2023-06-26 13:17:15 -0400

    added homepage text, changed sidebar color

--- chipReact/src/pages/Home.js
+++ chipReact/src/pages/Home.js
@@ -4,8 +4,13 @@ import Layout from '../components/Layout';
 const Home = () => {
     return (
         <Layout>
-            <h1>Home Page</h1>
-            <p>This is the content of the home page.</p>
+            <h1>Centralized Hub for Inventories Platform</h1>
+            <p>A simple way to create and consume SBOMs</p>
+            <h3>What are SBOMs?</h3>
+            <p>
+                A Software Bill of Materials (SBOM) is a formal record of all software packages and components used by a software project or machine. These components can be commercial or open-source, and in turn rely on other components themselves. This creates a series of reliances, or a chain of dependencies, that is not easily visible to the creator or user of the main project.
+                An SBOM helps to solve this issue by finding all the dependencies of a project and listing them out (in our case) as a JSON file. They’re an important part of maintaining cybersecurity, and as of an executive order issued on May 2021, are required for all software projects.
+            </p>
         </Layout>
     );
 };
