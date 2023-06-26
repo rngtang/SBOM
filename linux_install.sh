9a8a7cec21362c093e49e7f8e090c74cd2779c26
Judy Chen <jc942@duke.edu>
2023-06-26 16:14:14 -0400

    sudo required, also weird grype database thing, but linux script finished

--- linux_install.sh
+++ linux_install.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-
-echo "Hello!" 
-
 chmod +x linux_install.sh
 
-# curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh 
+echo "--- INSTALLING SYFT... ---" 
 wget https://raw.githubusercontent.com/anchore/syft/main/install.sh
-
 chmod +x install.sh
 ./install.sh
-
 sudo mv ./bin/syft /usr/local/bin
-syft -h 
+echo "--- Successful: INSTALLED SYFT ---"
 
-echo "INSTALLED SYFT"
+echo "--- INSTALLING GRYPE... ---" 
+wget https://raw.githubusercontent.com/anchore/grype/main/install.sh
+chmod +x install.sh.1
+./install.sh.1
+sudo mv ./bin/grype /usr/local/bin
+echo "--- Successful: INSTALLED GRYPE ---"
 
 
-echo "Hello! Which file would you like to create an SBOM for?"
+
+echo "Which file would you like to create an SBOM for?"
 read NAME
 echo "Creating $NAME.cdx.json ..."
 
-syft $NAME -o cyclonedx-json=$NAME.cdx.json
+syft $NAME -o cyclonedx-json=$NAME.sbom.json
+grype sbom:$NAME.sbom.json 
+# -o cyclonedx-json
 
-echo "You have now created $NAME.cdx.json
+echo "You have now created $NAME.sbom.json, which is your SBOM to upload. Your vulnerabilities are stored in the grype database and can be seen with <grype db status>"
