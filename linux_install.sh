ed4f10552ed3ca46befd0cda6531035cff200d9d
Judy Chen <jc942@duke.edu>
2023-06-26 15:10:57 -0400

    can now make cdx json

--- linux_install.sh
+++ linux_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hello world"
+echo "Hello!" 
 
 chmod +x linux_install.sh
 
@@ -10,8 +10,16 @@ wget https://raw.githubusercontent.com/anchore/syft/main/install.sh
 chmod +x install.sh
 ./install.sh
 
-cd bin 
-sudo mv ./syft /usr/local/bin
+sudo mv ./bin/syft /usr/local/bin
 syft -h 
 
 echo "INSTALLED SYFT"
+
+
+echo "Hello! Which file would you like to create an SBOM for?"
+read NAME
+echo "Creating $NAME.cdx.json ..."
+
+syft $NAME -o cyclonedx-json=$NAME.cdx.json
+
+echo "You have now created $NAME.cdx.json
