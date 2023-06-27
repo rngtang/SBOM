0d9d0c1304fa001b20fabe6dda5da1d8a6c6144c
Judy Chen <jc942@duke.edu>
2023-06-27 11:41:38 -0400

    fixed sudo?

--- linux_install.sh
+++ linux_install.sh
@@ -12,7 +12,7 @@ echo -e "${COLOR}--- INSTALLING SYFT... ---${NC}"
 wget https://raw.githubusercontent.com/anchore/syft/main/install.sh
 chmod +x install.sh
 ./install.sh
-sudo mv ./bin/syft /usr/local/bin
+# sudo mv ./bin/syft /usr/local/bin
 echo -e "${COLOR}--- Successful: INSTALLED SYFT ---${NC}"
 
 # pulls script to install grype into a new file called install.sh.1 and runs it 
@@ -20,7 +20,7 @@ echo -e "${COLOR}--- INSTALLING GRYPE... ---${NC}"
 wget https://raw.githubusercontent.com/anchore/grype/main/install.sh
 chmod +x install.sh.1
 ./install.sh.1
-sudo mv ./bin/grype /usr/local/bin
+# sudo mv ./bin/grype /usr/local/bin
 echo -e "${COLOR}--- Successful: INSTALLED GRYPE ---${NC}"
 
 # asks user to give the file to run 
@@ -29,8 +29,7 @@ echo -e "${COLOR}Which file would you like to create an SBOM for?${NC}"
 read NAME
 echo -e "${COLOR}Creating $NAME.sbom.json ...${NC}"
 
-syft $NAME -o cyclonedx-json=$NAME.sbom.json
-grype sbom:$NAME.sbom.json 
-# -o cyclonedx-json
+./bin/syft $NAME -o cyclonedx-json=$NAME.sbom.json
+./bin/grype sbom:$NAME.sbom.json 
 
 echo -e "${COLOR}You have now created $NAME.sbom.json, which is your SBOM to upload. Your vulnerabilities are stored in the grype database and can be seen with <grype db status>${NC}"
