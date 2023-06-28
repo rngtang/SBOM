a7796162cf2ed22e3c2869414510af6da761b5f2
Judy Chen <jc942@duke.edu>
2023-06-28 16:04:04 -0400

    working with ps1, problem are 2 lines that have to be done in terminal by user

--- windows_install.ps1
+++ windows_install.ps1
@@ -1,40 +1,8 @@
-# Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-Function Check-RunAsAdministrator()
-{
-  #Get current user context
-  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  
-  #Check user is running the script is member of Administrator Group
-  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
-  {
-       Write-host "Script is running with Administrator privileges!"
-  }
-  else
-    {
-       #Create a new Elevated process to Start PowerShell
-       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
- 
-       # Specify the current script path and name as a parameter
-       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
- 
-       #Set the Process to elevated
-       $ElevatedProcess.Verb = "runas"
- 
-       #Start the new elevated process
-       [System.Diagnostics.Process]::Start($ElevatedProcess)
- 
-       #Exit from the current, unelevated, process
-       Exit
- 
-    }
-}
- 
-#Check Script is running with Elevated Privileges
-Check-RunAsAdministrator
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+# Start-Process powershell -Verb RunAs 
 
-# RUN WITH: powershell.exe -File script.ps1 "path/to/selected/file.txt"
+# Code beyond this point will be executed in the elevated PowerShell session
 
-ForegroundColor Green
 # $selectedFile = $args[0]
 # Write-Host "Selected file: $selectedFile"
  
@@ -43,7 +11,7 @@ Write-Host "Hello World" -ForegroundColor Green
 # using syft to create a SBOM for a file in same directory
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # choco 
-choco install syft -y
+choco install syft -y --force
 
 Write-Host "Which file would you like to create an SBOM for?" -ForegroundColor Green
 $NAME= Read-Host 
@@ -53,4 +21,4 @@ syft $NAME -o cyclonedx-json=$NAME.sbom.json
 
 Write-Host "You have now created $NAME.sbom.json, which is your SBOM to upload." -ForegroundColor Green
 
-Read-Host -Prompt "Press Enter to exit" -ForegroundColor Green
+Read-Host -Prompt "Press Enter to exit" 
