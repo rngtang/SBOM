db8515d6c93fc08f6ede3bef1c35f1c928559a12
James Mu <jamesmu@Jamess-MacBook-Air.local>
2023-06-28 11:50:43 -0400

    logout page done

--- chipReact/src/pages/Logout.js
+++ chipReact/src/pages/Logout.js
@@ -1,4 +1,29 @@
-function Logout(){
-    return <div className='page'>Logout</div>
-}
+import React from 'react';
+import styles from './GenerateSBOMs.module.css';
+
+const LogoutMessage = () => {
+    return (
+        <div className={styles.section}> 
+        {/* stole the css from the generate page */}
+            <h3>You have successfully logged out!</h3>
+            <HomeButton />
+        </div>
+    );
+};
+
+const HomeButton = () => {
+    return (
+        <div className='page'>
+            <a href='/home'>Return Home</a>
+        </div>
+    );
+};
+
+const Logout = () => {
+    return (
+    <div className='page'>
+        <LogoutMessage />
+    </div>
+    );
+};
 export default Logout;
