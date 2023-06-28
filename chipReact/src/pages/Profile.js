857662a7ef757b345637b703d84c29b2d59e9ccb
James Mu <jamesmu@Jamess-MacBook-Air.local>
2023-06-28 14:23:19 -0400

    basic profile up and running

--- chipReact/src/pages/Profile.js
+++ chipReact/src/pages/Profile.js
@@ -1,4 +1,64 @@
-function Profile(){
-    return <div className='page'>Profile</div>
-}
+import React from 'react';
+import styles from './Profile.module.css';
+import user1 from '../data/user.json';
+
+const Profile = () => {
+  const { username, netid, platformSettings, profileInformation } = user1;
+
+  return (
+    <div className={styles.section}>
+      <div className={styles.profileContainer}>
+        <div className={styles.leftHeader}>
+          <h2>{username}</h2>
+          <p>{netid}</p>
+        </div>
+
+        <div className={styles.profileSection}>
+          <div className={styles.leftSection}>
+            <h4>Platform Settings</h4>
+            <div className={styles.toggleContainer}>
+              <label>
+                Email Updates
+                <input type="checkbox" checked={platformSettings.emailUpdates} readOnly />
+              </label>
+              <label>
+                Text Updates
+                <input type="checkbox" checked={platformSettings.textUpdates} readOnly />
+              </label>
+              <label>
+                Monthly Newsletter
+                <input type="checkbox" checked={platformSettings.monthlyNewsletter} readOnly />
+              </label>
+            </div>
+          </div>
+
+          <div className={styles.rightSection}>
+            <h4>Profile Information</h4>
+            <p>
+              <strong>Full Name: </strong>
+              {profileInformation.fullName}
+            </p>
+            <p>
+              <strong>Mobile Number: </strong>
+              {profileInformation.mobileNumber}
+            </p>
+            <p>
+              <strong>Email: </strong>
+              {profileInformation.email}
+            </p>
+            <p>
+              <strong> Position: </strong>
+              {profileInformation.position}
+            </p>
+            <p>
+              <strong> # of SBOMs made: </strong>
+              {profileInformation.sbomCounter}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Profile;
