2d602ec76054b224f0658107dfce02894a36568d
Caleb Kiprono <caleb.kiprono@duke.edu>
2023-06-29 11:05:50 -0400

    redirects to shib! Yay

--- chipReact/src/components/Login.js
+++ chipReact/src/components/Login.js
@@ -1,36 +1,47 @@
-import React, { useEffect, useContext } from 'react';
-import { UserContext } from '../UserContext';
+// import React, { useEffect, useContext } from 'react';
+// import { UserContext } from '../UserContext';
+
+// function Login() {
+//   const { setUser } = useContext(UserContext);
+
+//   useEffect(() => {
+//     fetch('https://shib.oit.duke.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=https://chip.duke.edu')
+//       .then((response) => {
+//         console.log(response);
+//         console.log(response.redirected);
+//         console.log(response.url);
+        
+//         if(response.redirected){
+//           window.location.href = response.url;
+//         }
+        
+//         //we're assuming we get the user data as json endpoint
+//         return response.json();
+//       })
+//       .then((data) => {
+//         setUser(data.user);  // Save the logged in user data to context
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Shib testing</h1>
+//     </div>
+//   );
+// }
+
+// export default Login;
+import { useEffect } from 'react';
 
 function Login() {
-  const { setUser } = useContext(UserContext);
-
   useEffect(() => {
-    fetch('https://shib.oit.duke.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=https://chip.duke.edu')
-      .then((response) => {
-        console.log(response);
-        console.log(response.redirected);
-        console.log(response.url);
-        
-        if(response.redirected){
-          window.location.href = response.url;
-        }
-        
-        //we're assuming we get the user data as json endpoint
-        return response.json();
-      })
-      .then((data) => {
-        setUser(data.user);  // Save the logged in user data to context
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+    window.location.href = 'https://shib.oit.duke.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=https://chip.duke.edu';  
   }, []);
 
-  return (
-    <div>
-      <h1>Shib testing</h1>
-    </div>
-  );
+  return null;
 }
 
 export default Login;
