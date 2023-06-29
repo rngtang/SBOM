2014355dcc6f22bbb0bcb1080aab54ce3a0111ae
Alice Hu <yh353@duke.edu>
2023-06-29 11:02:34 -0400

    added upload button

--- chipReact/src/pages/ViewSBOMs.js
+++ chipReact/src/pages/ViewSBOMs.js
@@ -2,13 +2,39 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyAccordian from '../components/MyAccordian.js';
 import styles from './GenerateSBOMs.module.css';
+import { Button } from 'react-bootstrap';
+import { useRef } from 'react';
+
 
 function ViewSBOMs() {
+  const fileInput = useRef();
+
+  const handleButtonClick = () => {
+    fileInput.current.click();
+  }
+
+  const handleFileUpload = event => {
+    const file = event.target.files[0];
+    console.log(file);  // You can process the uploaded file here
+  }
+
   return (
-  <div className={styles.section}>
-    <MyAccordian name={'SBOM #1'} meta={'this was imported from idk'}/>
-    <MyAccordian name={'SBOM #2'} meta={'bruh i dont even know'}/>
-  </div>
+    <>
+    <div className='page'>
+      <Button variant="primary" className={styles.top} onClick={handleButtonClick}>Upload File</Button>
+      <input 
+        type="file" 
+        style={{ display: 'none' }} 
+        ref={fileInput} 
+        onChange={handleFileUpload} 
+      />
+    </div>
+    <div className={styles.section}>
+      
+      <MyAccordian name={'SBOM #1'} meta={'this was imported from idk'}/>
+      <MyAccordian name={'SBOM #2'} meta={'bruh i dont even know'}/>
+    </div>
+    </>
   );
 }
 
