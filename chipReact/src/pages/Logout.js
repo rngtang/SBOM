98a5561f49598e2bdea1653c74f764a0a37b4b2d
James Mu <jamesmu@Jamess-MacBook-Pro.local>
2023-07-05 14:01:05 -0400

    logout button works now

--- chipReact/src/pages/Logout.js
+++ chipReact/src/pages/Logout.js
@@ -1,43 +1,23 @@
 import React from 'react';
-import styles from './GenerateSBOMs.module.css';
-import { useNavigate } from 'react-router-dom';
 
-const LogoutMessage = () => {
-    return (
-        <div className={styles.section}> 
-        {/* stole the css from the generate page */}
-            <h3>If you pressed Log Out, You have successfully logged out!</h3>
-            <HomeButton />
-        </div>
-    );
-};
-
-const HomeButton = () => {
+const LogoutButton = ({ setLoggedIn }) => {
+    const handleLogoutClick = () => {
+        setLoggedIn(false);
+    }
     return (
         <div className='page'>
-            <a href='/home'>Return Home</a>
+            <a href='/home' onClick={handleLogoutClick}>Log out</a>
         </div>
     );
 };
 
 
-const Logout = ({ setLoggedIn }) => {
-    const navigate = useNavigate();
-
-    const handleLogoutClick = () => {
-        // TODO: @calbe Replace with actual logout logic
-        setLoggedIn(false);
-        navigate("/home");
-    }
-
+const Logout = () => {
     return (
         <div>
-            <button onClick={handleLogoutClick}>Log out</button>
-            <LogoutMessage/>
+            <LogoutButton/>
         </div>
     );
 };
 
 export default Logout;
-
-
