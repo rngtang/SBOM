e6858b0fb2a011364adbb33054dfc76866e5673f
Raul <ren11@duke.edu>
2023-07-05 14:37:39 -0400

    installed cdxgen

--- linux_install.sh
+++ linux_install.sh
@@ -8,12 +8,14 @@ NC='\033[0m' # No Color
 # echo -e "${COLOR}Selected file: $selected_file${NC}" # Perform operations on the selected file
 
 # pulls script to install syft into a new file called install.sh and runs it 
-echo -e "${COLOR}--- INSTALLING SYFT... ---${NC}"  
+echo -e "${COLOR}--- INSTALLING CDXGEN... ---${NC}"  
 wget https://raw.githubusercontent.com/anchore/syft/main/install.sh
-chmod +x install.sh
-./install.sh
+# chmod +x install.sh
+# ./install.sh
+npm install -g @cyclonedx/cdxgen@8.6.0
+
 # sudo mv ./bin/syft /usr/local/bin
-echo -e "${COLOR}--- Successful: INSTALLED SYFT ---${NC}"
+echo -e "${COLOR}--- Successful: INSTALLED CDXGEN ---${NC}"
 
 # pulls script to install grype into a new file called install.sh.1 and runs it 
 echo -e "${COLOR}--- INSTALLING GRYPE... ---${NC}" 
@@ -27,9 +29,11 @@ echo -e "${COLOR}--- Successful: INSTALLED GRYPE ---${NC}"
 # needs to be precise name -> need to add error handling
 echo -e "${COLOR}Which file would you like to create an SBOM for?${NC}"
 read NAME
-echo -e "${COLOR}Creating $NAME.sbom.json ...${NC}"
+echo -e "${COLOR}Creating $NAME.json ...${NC}"
 
-./bin/syft $NAME -o cyclonedx-json=$NAME.sbom.json
-./bin/grype sbom:$NAME.sbom.json 
+cdxgen -o $NAME.json
+
+# ./bin/syft $NAME -o cyclonedx-json=$NAME.sbom.json
+grype sbom:./$NAME.json -o cyclonedx-json > grype.json
 
 echo -e "${COLOR}You have now created $NAME.sbom.json, which is your SBOM to upload. Your vulnerabilities are stored in the grype database and can be seen with <grype db status>${NC}"
