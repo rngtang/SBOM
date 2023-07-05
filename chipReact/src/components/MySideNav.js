ff5bc0e7a18c60caf2bc145c963079804195bd18
Judy Chen <jc942@duke.edu>
2023-07-05 11:41:13 -0400

    trying to fix hover

--- chipReact/src/components/MySideNav.js
+++ chipReact/src/components/MySideNav.js
@@ -16,29 +16,29 @@ const MySideNav = ({loggedIn}) => {
         </div>
         <ul className="nav flex-column">
           <li className="nav-item">
-            <NavLink to="/home" className="nav-link" activeClassName="active">
+            <NavLink to="/home" className="nav-link">
               Home
             </NavLink>
           </li>
           {loggedIn && (
             <>
               <li className="nav-item">
-                <NavLink to="/viewsboms" className="nav-link" activeClassName="active">
+                <NavLink to="/viewsboms" className="nav-link">
                   View SBOMs
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/generatesboms" className="nav-link" activeClassName="active">
+                <NavLink to="/generatesboms" className="nav-link">
                   Generate SBOMs
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/profile" className="nav-link" activeClassName="active">
+                <NavLink to="/profile" className="nav-link">
                   Profile
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/logout" className="nav-link" activeClassName="active">
+                <NavLink to="/logout" className="nav-link">
                   Logout
                 </NavLink>
               </li>
