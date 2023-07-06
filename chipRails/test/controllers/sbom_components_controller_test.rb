e52bb39ae20c44ce5826c2173d12556ac2ad2683
Judy Chen <jc942@duke.edu>
2023-07-06 16:15:16 -0400

    h

--- chipRails/test/controllers/sbom_components_controller_test.rb
+++ chipRails/test/controllers/sbom_components_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class dependencyTest < ActiveSupport::TestCase
+class sbomComponentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
--- chipRails/test/models/sbom_component_test.rb
+++ chipRails/test/models/sbom_component_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class dependencyrenControllerTest < ActionDispatch::IntegrationTest
+class sbomComponentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
