c345e8fc3cddb2b84f187ac7fdc41fc54a43c0d8
Raul <ren11@duke.edu>
2023-07-06 11:21:51 -0400

    sboms files

--- linux_install.sh
+++ linux_install.sh
@@ -26,4 +26,9 @@ cdxgen -r -o $selected_file.1.json
 # ./bin/syft $selected_file -o json=$selected_file.json
 echo -e "${COLOR} Finished creating $selected_file.json ${NC}"
 ./bin/grype sbom:$selected_file.1.json -o cyclonedx-json > $selected_file.2.json
+if [ -f $selected_file.1.xml ]; then
+    rm $selected_file.1.xml
+fi
+sudo apt-get install jq
+jq -s 'add' $selected_file.1.json $selected_file.2.json > result.json
 echo -e "${COLOR}You have now created $selected_file.json, which is your SBOM to upload. Your vulnerabilities are stored in the grype database and can be seen with <grype db status>${NC}"        
