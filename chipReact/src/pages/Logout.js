0651ba8780e19a24e18f62a1d0c5c01ce63560ca
Judy Chen <jc942@duke.edu>
2023-07-10 14:15:54 -0400

    doing logout page

--- chipReact/src/pages/Logout.js
+++ chipReact/src/pages/Logout.js
@@ -3,7 +3,10 @@ import React from 'react';
 const LogoutMessage = () => {
     return (
         <div>
-            <a href='/home'>you have logged out. press here to go home.</a>
+            <section className='text' style={styles.text}>
+                <h1>You have successfully logged out!</h1>
+                <a href='/home'>Back to home page.</a>
+            </section>
         </div>
     );
 };
@@ -17,6 +20,20 @@ const Logout = ({ setLoggedIn, setLoggingOut }) => {
             <LogoutMessage />
         </div>
     );
+};
+
+const styles = {
+    background: {
+    }, 
+    text: {
+        margin: 'auto',
+        textAlign: 'center',
+        border: '4px black solid',
+        padding: '16px',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(35%, -50%)'
+    }
 };
 
 export default Logout;
