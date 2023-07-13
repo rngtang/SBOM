1fd6d0c77a1af84acc9b66e4fcbbf69f32e44ea1
Caleb Kiprono <caleb.kiprono@duke.edu>
2023-07-13 13:26:09 -0400

    cookies almost

--- chipRails/app/controllers/application_controller.rb
+++ chipRails/app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def authenticate_user!
-    # unless current_user
-    #   redirect_to "https://shib.oit.duke.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=https://chip.duke.edu", allow_other_host: true
-    # end
+    unless current_user
+      redirect_to "http://localhost:3001", allow_other_host: true
+    end
   end
   
 end
