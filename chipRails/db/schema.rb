a5bf7906846a5206682f7118f6b599fbcc66a1ef
Raul <ren11@duke.edu>
2023-07-13 14:15:22 -0400

    archive default value added

--- chipRails/db/schema.rb
+++ chipRails/db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_175308) do
     t.integer "version"
     t.string "name"
     t.string "description"
-    t.boolean "archive"
+    t.boolean "archive", default: false, null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -94,7 +94,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_175308) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
