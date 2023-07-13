cf2489d8710e68b8535cfa4807472c6db08359f5
Judy Chen <jc942@duke.edu>
2023-07-13 14:16:46 -0400

    fixed?

--- chipRails/app/controllers/scripts_controller.rb
+++ chipRails/app/controllers/scripts_controller.rb
@@ -44,28 +44,25 @@ class ScriptsController < ApplicationController
 
     def windows
         ps_code = <<-SCRIPT
-        # Start-Process PowerShell -Verb runAs -ArgumentList "-NoExit 'C:\Users\87jud\OneDrive - Duke University\Documents\Code+\windows_install.ps1'"
-
         Write-Host "Hello World" -ForegroundColor Green
-        
+
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
         # choco 
         choco install syft -y --force
-        
+
         Write-Host "Which file would you like to create an SBOM for?" -ForegroundColor Green
         $selectedFile= Read-Host 
         Write-Host "Creating $selectedFile.json ..." -ForegroundColor Green
-        
+
         syft $selectedFile -o json=$selectedFile.json
-        
+
         Write-Host "You have now created $selectedFile.sbom.json, which is your SBOM to upload." -ForegroundColor Green
-        
+
         Read-Host -Prompt "Press Enter to exit script and terminal" 
-        
-        Exit        
+
+        Exit
         SCRIPT
         send_data ps_code, filename: 'windows_install.ps1', type: 'text/plain'
-
     end 
     
   end
