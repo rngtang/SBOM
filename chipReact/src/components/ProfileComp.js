b67ae9858e46681a7cd2d98826c1f42b48ad6950
Caleb Kiprono <caleb.kiprono@duke.edu>
2023-07-17 14:01:51 -0400

    prof

--- chipReact/src/components/ProfileComp.js
+++ chipReact/src/components/ProfileComp.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ProfileComp.css';
+import { data } from 'jquery';
 
 export default function ProfileComp() {
+    const [email, setEmail] = useState('');
+    const [netid, setNetid] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:8080/current_user')
+            .then(response => response.json())
+            .then(data => {
+                setEmail(data.email);
+                setNetid(data.netid);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <>
             <div id='profile'>
-                <p>Welcome NAME!</p>
-                <p>You are logged in as netID</p>
+                <p>Welcome {email}!</p>
+                <p>You are logged in as {netid}</p>
             </div>
         </>
     )
