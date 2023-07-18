d5cabefe5b7e826a0ec77187dea3660d2f1fd994
Judy Chen <jc942@duke.edu>
2023-07-18 16:29:45 -0400

    ready to merge development in

--- merge_script.sh
+++ merge_script.sh
@@ -7,38 +7,38 @@ git add .
 git commit -m "ready to merge development in"
 git push
 
-# get most updated development
-echo "get most updated development"
-git checkout development
-git pull
+# # get most updated development
+# echo "get most updated development"
+# git checkout development
+# git pull
 
-# go back to local branch
-echo "go back to local branch"
-branch=$(git symbolic-ref --short HEAD)
-git checkout $branch
+# # go back to local branch
+# echo "go back to local branch"
+# branch=$(git symbolic-ref --short HEAD)
+# git checkout $branch
 
-# merge development into local branch 
-echo "merge dev into local branch"
-git merge development
-
-
-# resolve any merge conflicts 
-echo "resolve any merge conflicts"
+# # merge development into local branch 
+# echo "merge dev into local branch"
+# git merge development
 
 
-# then commit merge conflict resolution
-echo "then commit merge conflict resolution"
-git add .
-git commit -m "merged development"
-git push
+# # resolve any merge conflicts 
+# echo "resolve any merge conflicts"
 
-# go back to development and merge local branch 
-echo "go back to dev and merge local"
-git checkout development 
-git merge $branch
 
-# update development 
-echo "update dev"
-git add .
-git commit -m "merged branch $branch" 
-git push
+# # then commit merge conflict resolution
+# echo "then commit merge conflict resolution"
+# git add .
+# git commit -m "merged development"
+# git push
+
+# # go back to development and merge local branch 
+# echo "go back to dev and merge local"
+# git checkout development 
+# git merge $branch
+
+# # update development 
+# echo "update dev"
+# git add .
+# git commit -m "merged branch $branch" 
+# git push
