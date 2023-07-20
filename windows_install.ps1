7db40af696cd1ddc93ac49acced064e1c56b39ad
Judy Chen <jc942@duke.edu>
2023-07-20 15:47:55 -0400

    testing out with node

--- windows_install.ps1
+++ windows_install.ps1
@@ -3,20 +3,21 @@ Write-Host "Hello World" -ForegroundColor Green
 # Start-Process PowerShell -Verb runAs
 
 # deno
-Write-Host "Installing deno" -ForegroundColor Green
-irm https://deno.land/install.ps1 | iex
+# Write-Host "Installing deno" -ForegroundColor Green
+# irm https://deno.land/install.ps1 | iex
 
-Write-Host "checking for deno" -ForegroundColor Green 
-deno --version
+# Write-Host "checking for deno" -ForegroundColor Green 
+# deno --version
 
 Write-Host "Installing cdxgen. This may take a while." -ForegroundColor Green
-deno install --allow-read --allow-env --allow-run --allow-sys --allow-write --allow-net -n cdxgen "npm:@cyclonedx/cdxgen" -f
+# deno install --allow-read --allow-env --allow-run --allow-sys --allow-write --allow-net -n cdxgen "npm:@cyclonedx/cdxgen" -f
+npm install -g @cyclonedx/cdxgen@8.6.0
 
 Write-Host "What is the name of the file would you like to create an SBOM for? This is the same as the name of the directory you are in." -ForegroundColor Green
 $selectedFile= Read-Host 
 
 Write-Host "Creating $selectedFile.json ..." -ForegroundColor Green 
-cdxgen -r -o $selected_file.json --allow-sys
+cdxgen -r -o $selected_file.json 
 Write-Host "You have now created $selectedFile.json, which is your SBOM to upload." -ForegroundColor Green
 
 Read-Host -Prompt "Press Enter to exit script and terminal" 
