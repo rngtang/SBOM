07285f55b28dc3a1a980b1fe3d2734c7215c5043
Judy Chen <jc942@duke.edu>
2023-07-20 13:47:01 -0400

    windows script not working

--- windows_install.ps1
+++ windows_install.ps1
@@ -1,11 +1,13 @@
 Write-Host "Hello World" -ForegroundColor Green
-# .\OneDrive - Duke University\Documents\Code+
 
 # Start-Process PowerShell -Verb runAs
 
 # deno
 Write-Host "Installing deno" -ForegroundColor Green
 irm https://deno.land/install.ps1 | iex
+
+Write-Host "checking for deno" -ForegroundColor Green 
+deno --version
 
 Write-Host "Installing cdxgen. This may take a while." -ForegroundColor Green
 deno install --allow-read --allow-env --allow-run --allow-sys --allow-write --allow-net -n cdxgen "npm:@cyclonedx/cdxgen" -f
@@ -15,8 +17,7 @@ $selectedFile= Read-Host
 
 Write-Host "Creating $selectedFile.json ..." -ForegroundColor Green 
 cdxgen -r -o $selected_file.json --allow-sys
-
-# Write-Host "You have now created $selectedFile.json, which is your SBOM to upload." -ForegroundColor Green
+Write-Host "You have now created $selectedFile.json, which is your SBOM to upload." -ForegroundColor Green
 
 Read-Host -Prompt "Press Enter to exit script and terminal" 
 
