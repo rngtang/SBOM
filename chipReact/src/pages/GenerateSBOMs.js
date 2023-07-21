9bb13f3032d0beaaf5666e64596722cedfdcbe5d
Judy Chen <jc942@duke.edu>
2023-07-21 09:49:17 -0400

    finished formatting

--- chipReact/src/pages/GenerateSBOMs.js
+++ chipReact/src/pages/GenerateSBOMs.js
@@ -3,8 +3,8 @@ import styles from './GenerateSBOMs.module.css';
 import CodeBox from '../components/CodeBox';
 import prereq1 from '../data/prereq.json';
 import data1 from '../data/linux.json';
-import data2 from '../data/windows.json';
-import data3 from '../data/mac.json';
+import data2 from '../data/mac.json';
+import data3 from '../data/windows.json';
 import DownloadLinuxScript from '../components/DownloadLinuxScript';
 import DownloadMacScript from '../components/DownloadMacScript';
 import DownloadWindowsScript from '../components/DownloadWindowsScript';
@@ -31,48 +31,27 @@ const Prereq = ({ title, para1 }) => {
 const Section = ({ title, notice, text, code, downloadComponent }) => {
     const [isCopied, setIsCopied] = useState(false);
     const steps = Array.isArray(text) ? text : [text];
-    const warnings = Array.isArray(notice) ? notice : [notice];
-
+  
     const renderSteps = (steps) => {
-    if (Array.isArray(steps)) {
-        return steps.map((step, index) => {
-            if (Array.isArray(step)) {
-                // render sublist
-                return (
-                    <ul key={index} className={styles.sublist}>
-                        {renderSteps(step)}
-                    </ul>
-                );
-            } else {
-                return (
-                    <li key={index} className={styles.step}>{step}</li>
-                );
-            } 
-        });
-    } else {
-        // Render a single list item for the single string
-        return (
-            <li className={styles.step}>{steps}</li>
-        );
-        }
+      return steps.map((step, index) => (
+        <li key={index} className={styles.step}>{step}</li>
+      ));
     };
-
+  
     return (
-        <div className={styles.section}>
-            <div>
-                <h2 className={styles.h2}>{title}</h2>
-                <ul>
-                    {renderSteps(warnings)}
-                </ul>
-                <ol>
-                    {renderSteps(steps)}
-                </ol>
-                {downloadComponent && downloadComponent}
-                <CodeBox text={code} />
-            </div>
+      <div className={styles.section}>
+        <div>
+          <h2 className={styles.h2}>{title}</h2>
+          {notice && <li className={styles.warnings}>{notice}</li>}
+          <ol>
+            {renderSteps(steps)}
+          </ol>
+          {downloadComponent && downloadComponent}
+          <CodeBox text={code} />
         </div>
+      </div>
     );
-};
+  };
 
 // create the page that actually gets returned
 const AnotherPage = () => {
@@ -80,8 +59,8 @@ const AnotherPage = () => {
         <>
             <Prereq title={prereq1.title} para1={prereq1.para1} docker={prereq1.docker} />
             <Section title={data1.title} notice={data1.notice} text={data1.text} code={data1.code} downloadComponent={<DownloadLinuxScript />} />
-            <Section title={data3.title} notice={data2.notice} text={data3.text} code={data3.code} downloadComponent={<DownloadMacScript />} />
-            <Section title={data2.title} notice={data3.notice} text={data2.text} code={data2.code} downloadComponent={<DownloadWindowsScript />} />
+            <Section title={data2.title} text={data2.text} code={data2.code} downloadComponent={<DownloadMacScript />} />
+            <Section title={data3.title} notice={data3.notice} text={data3.text} code={data3.code} downloadComponent={<DownloadWindowsScript />} />
         </>
     );
 };
