d647da5b71d95f80dcad0466067a13d59697ac07
Judy Chen <jc942@duke.edu>
2023-07-21 08:40:56 -0400

    added comment

--- windows_install.ps1
+++ windows_install.ps1
@@ -1,11 +1,14 @@
+# automatically uses name of directory (Project name) as the name of SBOM
 param (
     [Parameter(Mandatory=$false)]
     [string]$selectedFile = (Get-Item $PSScriptRoot).BaseName
 )
 
+# install cdxgen. assume they have node/npm
 Write-Host "Installing cdxgen. This may take a while." -ForegroundColor Green
 npm install -g @cyclonedx/cdxgen@8.6.0
 
+# run cdxgen to create sbom
 Write-Host "Creating $selectedFile.json..." -ForegroundColor Green 
 cdxgen -r -o "${selectedFile}.json"
 
@@ -15,8 +18,9 @@ if (Test-Path $xmlFile -PathType Leaf) {
     Remove-Item $xmlFile
 }
 
+# finish
 Write-Host "You have now created $selectedFile.json, which is your SBOM to upload." -ForegroundColor Green
 
+# exit the terminal 
 Read-Host -Prompt "Press Enter to exit script and terminal" 
-
 Exit
