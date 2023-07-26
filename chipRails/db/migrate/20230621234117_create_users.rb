695446fc73cbacc54dd8fdef81f1d9b47ad77456
Raul <ren11@duke.edu>
2023-07-26 15:39:02 -0400

    database final version

--- chipRails/db/migrate/20230621234117_create_users.rb
+++ chipRails/db/migrate/20230621234117_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :email
       t.string :netid
-      # t.string :username
+      t.string :username
       t.timestamps
     end
   end
