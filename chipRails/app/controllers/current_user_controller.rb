963e90eb7b595c73cabb477929c76f140ccd2327
Caleb Kiprono <caleb.kiprono@duke.edu>
2023-07-26 13:52:03 -0400

    done

--- chipRails/app/controllers/current_user_controller.rb
+++
@@ -1,9 +0,0 @@
-class CurrentUserController < ApplicationController
-    def index
-      if current_user
-        render json: current_user
-      else
-        render json: {}, status: 404
-      end
-    end
-  end
