20654f2a9fb222ab0abba5af916bbcdf50a7a1e9
Alice Hu <yh353@duke.edu>
2023-07-26 22:21:54 -0400

    added custom nodes

--- chipReact/src/pages/tree-rendering/SbomTree.js
+++ chipReact/src/pages/tree-rendering/SbomTree.js
@@ -1,5 +1,3 @@
-// Axios functional code below:
-
 import React, { useState, useEffect } from 'react';
 import Tree from 'react-d3-tree';
 import axios from 'axios';
@@ -14,15 +12,16 @@ function SbomTree() {
   const [data, setData] = useState(null);
   const { sbomId } = useParams();
 
-  const generateNodeProps = () => {
-    return {
-      circleProps: { 
-        r: 10, 
-        fill: 'blue'
-      }
-    };
-  };
-  
+  const renderCustomNodeElement = ({ nodeDatum, toggleNode }) => (
+    <g>
+      <circle r={45} style={{ fill: '#B0E0E6' }} onClick={toggleNode} />
+      <foreignObject x="-60" y="-15" width="300" height="30"> {/* Adjust the dimensions here */}
+        <div style={{ textAlign: 'center', backgroundColor: '#f0f0f0', borderRadius: '5px', padding: '2px', border: '1px solid black' }}> {/* Add a border here */}
+          <span>{nodeDatum.name}</span>
+        </div>
+      </foreignObject>
+    </g>
+  );
 
   useEffect(() => {
     const fetchTreeData = async () => {
@@ -34,8 +33,8 @@ function SbomTree() {
 
   return (
     <div style={containerStyles}>
-      {data && <Tree data={data} translate={{ x: 400, y: 200 }} generateNodeProps={generateNodeProps} /> }
-      
+      {data && <Tree data={data} translate={{ x: 400, y: 200 }} separation={{ siblings: 1.3, nonSiblings: 2 }} depthFactor={800} renderCustomNodeElement={renderCustomNodeElement} pathFunc='elbow' />} {/* Increase the depthFactor here */}
+
     </div>
   );
 }
