c1f7c62527ad44c505b7f8bcde6d6d3f6b56a2f3
Alice Hu <yh353@duke.edu>
2023-07-27 11:42:42 -0400

    improved treevis

--- chipReact/src/pages/tree-rendering/SbomTree.js
+++ chipReact/src/pages/tree-rendering/SbomTree.js
@@ -2,38 +2,64 @@ import React, { useState, useEffect } from 'react';
 import Tree from 'react-d3-tree';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import samplejson from './sample.json'
 
 const containerStyles = {
   width: '100%',
   height: '800px',
 };
 
+
 function SbomTree() {
   const [data, setData] = useState(null);
   const { sbomId } = useParams();
 
+  const assignDepth = (node, depth = 0) => {
+    node.depth = depth;
+    if (node.children) {
+      node.children.forEach(child => assignDepth(child, depth + 1));
+    }
+  };
+
+  const getColorByDepth = (depth) => {
+    switch (depth) {
+      case 0:
+        return '#565676';
+      case 1:
+        return '#A76571';
+      case 2:
+        return '#C38D94';
+      default:
+        return '#ee6c4d';
+    }
+  };
+  
   const renderCustomNodeElement = ({ nodeDatum, toggleNode }) => (
     <g>
-      <circle r={45} style={{ fill: '#B0E0E6' }} onClick={toggleNode} />
-      <foreignObject x="-60" y="-15" width="300" height="30"> {/* Adjust the dimensions here */}
-        <div style={{ textAlign: 'center', backgroundColor: '#f0f0f0', borderRadius: '5px', padding: '2px', border: '1px solid black' }}> {/* Add a border here */}
-          <span>{nodeDatum.name}</span>
+      <circle r={85} style={{ fill: getColorByDepth(nodeDatum.depth) }} onClick={toggleNode} />
+      <foreignObject x="-60" y="-35" width="120" height="80">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', backgroundColor: '#f0f0f0', borderRadius: '5px', padding: '2px', border: '1px solid black' }}>
+          <span style={{ fontSize: '20px', fontFamily: 'Times New Roman' }}>{nodeDatum.name}</span>
         </div>
       </foreignObject>
     </g>
-  );
+  );  
+
 
   useEffect(() => {
     const fetchTreeData = async () => {
-      const result = await axios(`http://localhost:8080/sboms/${sbomId}/dependencies_tree`);
-      setData(result.data);
+      //const result = await axios(`http://localhost:8080/sboms/${sbomId}/dependencies_tree`);
+      const result = samplejson;
+      const treeData = result;
+      assignDepth(treeData);
+      setData(treeData);
     };
     fetchTreeData();
   }, [sbomId]);
 
   return (
     <div style={containerStyles}>
-      {data && <Tree data={data} translate={{ x: 400, y: 200 }} separation={{ siblings: 1.3, nonSiblings: 2 }} depthFactor={800} renderCustomNodeElement={renderCustomNodeElement} pathFunc='elbow' />} {/* Increase the depthFactor here */}
+      {data && <Tree data={data} translate={{ x: 400, y: 200 }} separation={{ siblings: 1.5, nonSiblings: 3 }} depthFactor={800} renderCustomNodeElement={renderCustomNodeElement} pathFunc='disjointelbow' />} {/* Increase the depthFactor here */}
 
     </div>
   );
