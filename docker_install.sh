bcfa78b5aa0e4a3c85c20cd300b5915f9700bb24
Raul <ren11@duke.edu>
2023-07-28 13:57:11 -0400

    docker image

--- docker_install.sh
+++ docker_install.sh
@@ -13,42 +13,9 @@ echo -e "${COLOR} Project (directory) name: ${NC} $selected_file"
 
 echo -e "${COLOR}--- ** Checking CDXGEN installation... ---${NC}"
 
-if which cdxgen >/dev/null 2>&1; then
-    echo -e "${COLOR}--- CDXGEN is already installed. Skipping re-installation... ---${NC}"
-else
-    echo -e "${COLOR}--- Cdxgen was not found. INSTALLING CDXGEN... ---${NC}"
-    # see if way to install without sudo
-    # mkdir -p ./bin
-    # npm install --prefix ./bin @cyclonedx/cdxgen@8.6.0
-    sudo npm install -g @cyclonedx/cdxgen@8.6.0
-    echo -e "${COLOR}--- Successful: INSTALLED CDXGEN ---${NC}"
-fi
-
 # Generate Software Bill of Materials (SBOM) for the selected file
 echo -e "${COLOR} Creating an SBOM for your file. This may take a while... ${NC}"
 cdxgen -r -o $selected_file.1.json
-
-if [ -x "./bin/grype" ]; then
-    echo -e "${COLOR}--- GRYPE is already installed. Skipping re-installation... ---${NC}"
-else
-    echo -e "${COLOR}--- Grype was not found. INSTALLING GRYPE... ---${NC}"
-    wget https://raw.githubusercontent.com/anchore/grype/main/install.sh
-    chmod +x install.sh
-    ./install.sh
-    echo -e "${COLOR}--- Successful: INSTALLED GRYPE ---${NC}"
-fi
-
-echo -e "${COLOR}--- ** CHECKING JQ INSTALLATION... ---${NC}"
-
-if [ -x "./jq" ]; then
-    echo -e "${COLOR}--- JQ is already installed. Skipping re-installation... ---${NC}"
-else
-    echo -e "${COLOR}--- JQ was not found. INSTALLING JQ... ---${NC}"
-    wget -O jq https://github.com/stedolan/jq/releases/latest/download/jq-linux64
-    chmod +x jq
-    echo -e "${COLOR}--- Successful: INSTALLED JQ ---${NC}"
-fi
-
 
 # Run Grype to analyze the SBOM
 echo -e "${COLOR} Finished running cdxgen on $selected_file. Running grype... ${NC}"
@@ -60,16 +27,16 @@ if [ -f $selected_file.1.xml ]; then
 fi
 
 # Combine the JSON files into one
-jq -s 'add' $selected_file.1.json $selected_file.2.json > $selected_file.linux.json
+./jq -s 'add' $selected_file.1.json $selected_file.2.json > $selected_file.SBOM.json
 
 # remove the extra .1 file created
-# if [ -f $selected_file.1.json ]; then
-#     rm $selected_file.1.json
-# fi
+if [ -f $selected_file.1.json ]; then
+    rm $selected_file.1.json
+fi
 # remove the extra .2 file created
-# if [ -f $selected_file.2.json ]; then
-#     rm $selected_file.2.json
-# fi
+if [ -f $selected_file.2.json ]; then
+    rm $selected_file.2.json
+fi
 
 # Output the results
-echo -e "${COLOR}You have now created $selected_file.linux.json, which is your SBOM to upload. Your vulnerabilities are stored in the grype database and can be seen with <grype db status>${NC}" 
+echo -e "${COLOR}You have now created $selected_file.SBOM.json, which is your SBOM to upload. Your vulnerabilities are stored in the grype database and can be seen with <grype db status>${NC}" 
--- dockerfile
+++ dockerfile
@@ -1,7 +1,12 @@
-FROM rngtang/chip-image
+FROM node:14
+
+# Install cdxgen, grype, and jq
+RUN npm install -g @cyclonedx/cdxgen@8.6.0
+RUN wget https://raw.githubusercontent.com/anchore/grype/main/install.sh && chmod +x install.sh && ./install.sh
+RUN wget -O ./jq https://github.com/stedolan/jq/releases/latest/download/jq-linux64 && chmod +x jq
 
 WORKDIR /app
 
-COPY . .
+COPY docker_install.sh .
 
 CMD ["./docker_install.sh"]
