5070756bc90fcd321f5222ecdc0070f1d038e12a
Raul <ren11@duke.edu>
2023-07-31 16:53:02 -0400

    sbom files cleaned

--- testjq.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-chmod +x "$0" 
-echo "hello world"
-
-jq '{ vulnerabilities: .vulnerabilities, components: .components}' sbom.2.json > new_sbom.json
-# jq -s 'add' sbom.1.json new_sbom.json > final.SBOM.json
