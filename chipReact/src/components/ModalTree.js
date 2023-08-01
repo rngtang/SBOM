09f4d577e20ca9d2162ab0eb3d6bc1d41a26c009
Judy Chen <jc942@duke.edu>
2023-08-01 12:10:32 -0400

    added to modal

--- chipReact/src/components/ModalTree.js
+++ chipReact/src/components/ModalTree.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import ModalBody from 'react-bootstrap/ModalBody'
 import './ModalTree.css'
 
 function ModalTree(props) {
@@ -13,11 +14,23 @@ function ModalTree(props) {
                 centered
             >
                 <Modal.Title id="contained-modal-title-vcenter">
-                    Dependency Tree generator alert. 
+                    Dependency Tree generator alert: 
                 </Modal.Title>
+                
                 <Modal.Body>
                     <p>The following Dependency Tree might not have all the transitive dependencies of your software.</p>
+                    <p>Currently, generating a Dependency Tree is limited to the following projects:</p>
+                    <ul>
+                        <li>package-lock.json</li>
+                        <li>yarn.lock</li>
+                        <li>pnpm-lock.yaml</li>
+                        <li>Maven (pom.xml)</li>
+                        <li>Gradle</li>
+                        <li>Python (requirements.txt, setup.py, pyproject.toml, poetry.lock)</li>
+                    </ul>
+                    <p> To learn more, go here: <a href="https://github.com/CycloneDX/cdxgen">https://github.com/CycloneDX/cdxgen</a> </p>
                 </Modal.Body>
+                
                 <Modal.Footer>
                     <Button onClick={props.onHide}>I understand</Button>
                 </Modal.Footer>
